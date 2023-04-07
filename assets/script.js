c9f78bf9438664e07dc340e05da95829e61eeb8e
[Ashley Braxton] <[ashley.braxton25@gmail.com]>
2023-04-06 21:36:54 -0400

    cleaned up code, added comments, edited readme

--- assets/script.js
+++ assets/script.js
@@ -6,17 +6,11 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword () {
   includedArr = [];
-  // var availableCharacters = {
-  //   hasLower: includeLowercase,
-  //   hasUper: includeUppercase,
-  //   hasNumbers: includeNumbers,
-  //   hasSymbol: includeSymbols, 
-  // }
- 
 
 
 
 
+//set the character types as strings and then split them into individual units to use in password generator
 var numbers = '123456789';
 numbers= numbers.split('')
 var symbols= '!@$_?%+&*#'
@@ -26,29 +20,30 @@ lowercase= lowercase.split('')
 var uppercase= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 uppercase= uppercase.split('')
 
-
+//asks user how many characters they would like, if they leave field blank or choose number not within acceptable range it returns nothing and tells them what needs to change
 const passLength = parseInt(prompt("How many characters would you like in your password? (must be at least 8 characters)"));
 if (!passLength) {
-  alert('Please choose a password length');
+  alert('Please choose a password length')
+  return '';
 
 }else if (passLength < 8 || passLength > 128) {
   alert('Password Length must be between 8 and 128 characters long')
+  return '';
 }else {
 const includeLowercase = confirm("Would you like to use lowercase letters?");
 const includeUppercase = confirm("Would you like to use uppercase Letters?");
 const includeNumbers = confirm("Would you like to use numbers?");
 const includeSymbols = confirm("Would you like to use special characters?");
+//^^^lets user personalize what types of charaters they would like in their password
 
-
-
+//If they do not choose any charater types it returns nothing and tells them they must select atleast one type
 if (!includeLowercase && !includeUppercase && !includeNumbers && !includeSymbols){
   alert('You must pick at least 1 character type to include')
   return '';
 }
-// const createPassword = (passLength, includeNumbers, includeLowercase, includeSymbols, includeUppercase) => {
   
 
-
+//based on user input, creates an array using only character types they would like in their password
 if (includeLowercase) {
 includedArr = includedArr.concat(lowercase);
 }
@@ -67,24 +62,16 @@ includedArr = includedArr.concat(numbers)
 if (includeSymbols) {
 includedArr = includedArr.concat(symbols) ;
 }
-// console.log(createPassword)
-console.log(includedArr)
-console.log(passLength)
-// let includedArr = '';
+//uses the math function to create a randomly generated array the Users desires length using their chosen Character types
 var randompass = []
-// console.log(createPassword((passLength, includeNumbers, includeLowercase, includeSymbols, includeUppercase)))
 for (var i = 0; i < passLength; i++) {
   var passIndex = Math.floor(Math.random()*includedArr.length);
   var randomChar = includedArr[passIndex];
   randompass.push (randomChar)
-  // var random = Math.floor(Math.random()*includedArr.length);;
-// }
 
 }
-console.log(randompass)
 return randompass.join('')
-//  }
-
+//random array becomes the password
 
 
 
@@ -93,12 +80,7 @@ return randompass.join('')
 
 }
 }
-// const availableCharacters = [
-  //   ...(includeNumbers ? numbers : []),
-  //   ...(includeLowercase ? lowercase : []),
-  //   ...(includeSymbols ? symbols : []),
-  //   ...(includeUppercase ? uppercase : []),
-  // ] ;
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -108,4 +90,4 @@ function writePassword() {
 
 }
 // Add event listener to generate button
-generateBtn.addEventListener( 'click', writePassword ());
+generateBtn.addEventListener( 'click', writePassword);
